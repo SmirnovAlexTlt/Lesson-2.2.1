f62b3d45ed8aa191799ccf8d192f210b21c60b03
AlexTLT <SmirnovAVtlt@yandex.ru>
2022-02-03 15:41:37 +0400

    Task complete

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,12 +19,17 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn (name = "id")
+   private Car carDetails;
+
    public User() {}
    
-   public User(String firstName, String lastName, String email) {
+   public User (String firstName, String lastName, String email,Car carDetails) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.carDetails = carDetails;
    }
 
    public Long getId() {
@@ -57,5 +62,13 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCarDetails() {
+      return carDetails;
+   }
+
+   public void setCarDetails(Car carDetails) {
+      this.carDetails = carDetails;
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,6 +1,7 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class UserServiceImp implements UserService {
    @Autowired
    private UserDao userDao;
 
+
+
    @Transactional
    @Override
    public void add(User user) {
@@ -25,5 +28,12 @@ public class UserServiceImp implements UserService {
    public List<User> listUsers() {
       return userDao.listUsers();
    }
+
+   @Override
+   @Transactional
+   public User getUser(String model, int series) {
+      return userDao.getUser(model,series);
+   }
+
 
 }
